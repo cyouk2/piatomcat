5e043ceb08ccd0683f21a10924471e135ffce6b1
cyouk2 <cyouk2@OH1144.jpn.ithd.local>
2016-10-03 14:57:32 +0900

    てｓｔ

--- WebContent/resources/v_satama_all_tai.sql
+++ WebContent/resources/v_satama_all_tai.sql
@@ -5,11 +5,18 @@ CREATE
 VIEW `v_satama_all_tai` AS
     SELECT DISTINCT
         `v`.`playDate` AS `playdate`,
+        IFNULL((SELECT 
+                        SUM(`p`.`ballOutput`)
+                    FROM
+                        `piainfo` `p`
+                    WHERE
+                        (`p`.`playDate` <= `v`.`playDate`)),
+                0) AS `totalOutBefore`,
         (SELECT 
                 SUM(`p`.`ballOutput`)
             FROM
                 `piainfo` `p`
             WHERE
-                (`p`.`playDate` <= `v`.`playDate`)) AS `totalOut`
+                (`p`.`playDate` = `v`.`playDate`)) AS `totalOut`
     FROM
         `piainfo` `v`
